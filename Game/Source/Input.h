eb54335f6104f1b8825277461b6a9705509c4262
adrianam4 <61042878+adrianam4@users.noreply.github.com>
2021-04-09 16:45:09 +0200

    gamepad support

--- Game/Source/Input.h
+++ Game/Source/Input.h
@@ -5,7 +5,11 @@
 
 //#define NUM_KEYS 352
 #define NUM_MOUSE_BUTTONS 5
+#define MAX_PADS 4
 //#define LAST_KEYS_PRESSED_BUFFER 50
+
+struct _SDL_GameController;
+struct _SDL_Haptic;
 
 struct SDL_Rect;
 
@@ -23,6 +27,26 @@ enum KeyState
 	KEY_DOWN,
 	KEY_REPEAT,
 	KEY_UP
+};
+
+struct GamePad
+{
+	//Input data
+	bool start, back, guide;
+	bool x, y, a, b, l1, r1, l3, r3;
+	bool up, down, left, right;
+	float l2, r2;
+	float l_x, l_y, r_x, r_y, l_dz, r_dz;
+
+	//Controller data
+	bool enabled;
+	int index;
+	_SDL_GameController* controller;
+	_SDL_Haptic* haptic;
+
+	//Rumble controller
+	int rumble_countdown;
+	float rumble_strength;
 };
 
 class Input : public Module
@@ -47,6 +71,19 @@ public:
 	// Called before quitting
 	bool CleanUp();
 
+	// Activates SDL device funcionallity when a gamepad has been connected
+	void HandleDeviceConnection(int index);
+
+	// Deactivates SDL device funcionallity when a gamepad has been disconnected
+	void HandleDeviceRemoval(int index);
+
+	// Called at PreUpdate
+    // Iterates through all active gamepads and update all input data
+	void UpdateGamepadsInput();
+
+	bool ShakeController(int id, int duration, float strength = 0.5f);
+	const char* GetControllerName(int id) const;
+
 	// Check key states (includes mouse and joy buttons)
 	KeyState GetKey(int id) const
 	{
@@ -69,6 +106,7 @@ private:
 	bool windowEvents[WE_COUNT];
 	KeyState*	keyboard;
 	KeyState mouseButtons[NUM_MOUSE_BUTTONS];
+	GamePad pads[MAX_PADS];
 	int	mouseMotionX;
 	int mouseMotionY;
 	int mouseX;
