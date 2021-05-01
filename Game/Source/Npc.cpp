0441ea1baf2f21535f57d5f2f542f347116cd21f
LucasPG14 <lucaspg14@gmail.com>
2021-05-01 21:05:57 +0200

    Fix

--- Game/Source/Npc.cpp
+++ Game/Source/Npc.cpp
@@ -144,7 +144,6 @@ bool Npc::CheckCollision(Player* player)
 
 			if (app->input->GetKey(SDL_SCANCODE_O) == KEY_DOWN || app->input->pad->GetButton(SDL_CONTROLLER_BUTTON_A) == KEY_DOWN)
 			{
-				QuestManager::GetInstance()->CheckQuests(type);
 				return true;
 				state = EntityState::TALKING;
 			}
